653aec2fd919993d388290205d7f49060e67df55
Arash-hqz <ghaffariarash369@gmail.com>
2025-02-15 12:19:41 +0330

    push loading commponent

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,12 @@
-const Loader = () => (
-  <div>Loader</div>
+import { loader } from "../assets";
+
+
+const Loader = ({title}) => (
+  <div className=" w-full flex justify-center items-center flex-c">
+    
+    <img src={loader} alt="loader" className=" w-32 h-32 object-contain" />
+    <h1></h1>
+  </div>
 );
 
 export default Loader;
