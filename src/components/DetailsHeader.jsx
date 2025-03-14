b665c07be30b1d36ecebf125b949e93129b07dd5
Arash-hqz <ghaffariarash369@gmail.com>
2025-03-14 22:19:11 +0330

    add details header

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,31 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from "react-router-dom";
+
+
+const DetailsHeader = ({ artistData , songData , artistId}) => {
+
+   const artist =  artistData?.artists[artistId]?.attributes;
+
+  return(
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+
+    <div className="flex inset-0 absolute items-center">
+
+      <img
+       src={artistId ? artist.artwork?.url
+        .replace('{h}' , '500').replace('{w}' , '500'):songData?.images?.coverart}
+       alt="art"
+       className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+       />
+
+       <div className="ml-5">
+        <p className=" font-bold text-white sm:text-3xl text-xl "> {artistId ? artist.name : songData?.title} </p>
+       </div>
+
+    </div>
+  </div>
+  )
+
+  };
 
 export default DetailsHeader;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,15 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { DetailsHeader , Error , Loader , RelatedSongs } from "../components";
 import { setActiveSong  , playPause } from "../redux/features/playerSlice";
+import { usegetSongDetail } from "../redux/service/ShazamCore";
 // import PlayPause from "../components/PlayPause";
 
 
 
 const SongDetails = () => {
- 
+  const dispatch = useDispatch();
   const { songid } = useParams();  
+  const { activeSong , isPlaying } = useSelector((state) => state.player);
+  const { data : songData , isFetching : isFetchingSongDetails } = usegetSongDetail({songid});
 
-  console.log(songid);
+//   console.log(songid);
   return(
         <div className="flex flex-col">
             {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
@@ -18,7 +21,11 @@ const SongDetails = () => {
            <div className="mb-10">
             <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
 
-            <div className="mt-5"></div>
+            <div className="mt-5">
+               {songData?.section[1].type === 'LYRICS' ? songData?.section[1].text.map((line,i) =>(
+                  <p className="text-gray-400 text-base my-1">  {line} </p>
+               )):    <p className="text-gray-400 text-base my-1">  Sorry , no lyrics found </p> } 
+            </div>
            </div>
 
         </div>
