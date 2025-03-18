5ed459bb8516b0917f6b45a3fa4f879ee40463d7
Arash-hqz <ghaffariarash369@gmail.com>
2025-03-18 08:26:07 +0330

    base api code add

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,9 +1,27 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Error , Loader , SongCard } from "../components";
 
 
-const AroundYou  = () => <div>CountryTracks</div>;
+const AroundYou  = () => {
+
+ const [country , setCountry] = useState();
+ const [loading , setLoading] = useState(true);
+ const [activeSong , isPlaying] = useSelector((state) => state.player );
+
+ useEffect(() =>{
+    axios.get('')
+    // از سایت geo شحه باید بگیریم برایه تشخیص ورود دستگاه از ای پی کدوم کشور هست
+    .then((res) => setCountry(res?.data?.locaton?.country))
+    .catch((err) => console.log(err) )
+    .finally(( ) => setLoading(false))
+ } , [country])
+
+ return(
+      
+      <div>CountryTracks</div>
+ );
+};
 
 export default  AroundYou ;
