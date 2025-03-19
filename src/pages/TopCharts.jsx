11f1eb2976d9731a3c8179da07f035c83bd42f29
Arash-hqz <ghaffariarash369@gmail.com>
2025-03-19 04:44:50 +0330

    TopCharts finish

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,47 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useEffect,  } from "react";
+import { useSelector } from "react-redux";
+import { Error , Loader , SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/service/ShazamCore";
 
-export default TopCharts;
+
+const TopCharts  = () => {
+
+ 
+//  const [activeSong , isPlaying] = useSelector((state) => state.player );
+//  const [ data , isFetching , error] = useGetTopChartsQuery(country);
+
+
+
+//  if (isFetching) {
+//     <Loader title="Loading songs Top Charts"/>
+//  }
+//  if (error && country) {
+//    <Error/>
+//  }
+
+ return(
+      
+      <div className="flex flex-col">
+         <h2 className="font-bold text-3xl text-white text-left mb-10 mt-4">
+           Discover Top Charts
+            {/* <span className="text-black">{country}</span> */}
+         </h2>
+         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {/* {data.map(( song , i)=>(
+               <SongCard
+               key={song.Key}
+               song={song}
+               isPlaying={isPlaying}
+               activeSong={activeSong}
+               data={data}
+               i={i}
+
+                />
+            ))} */}
+         </div>
+      </div>
+ );
+};
+
+export default  TopCharts ;
+
