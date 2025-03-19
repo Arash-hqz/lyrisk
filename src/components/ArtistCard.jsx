dde64e87cd87a3533b4f5826b0e51d6f063c0cdb
Arash-hqz <ghaffariarash369@gmail.com>
2025-03-19 05:40:51 +0330

    artists Card component developet

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,19 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+
+
+const ArtistCard = ({track}) => {
+  
+  const navigate = useNavigate(); 
+  
+  return(
+    <div className=" flex flex-col w-[250px] p4 bg-opacity-80 backdrop-blur-sm animate-slideup cursor-pointer"
+         onClick={() => navigate(`/artists/${track?.artists[0]?.adamid}`)} >
+      <img className=" w-full h-56 rounded-lg" src={traks?.images?.cover} alt="" />
+      <p className=" font-semibold mt-4 text-lg text-white truncate"> {track?.subtitle}</p>
+    </div>
+
+  );
 };
 
 export default ArtistCard;
--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,47 @@
-const TopArtists = () => <div>TopArtists</div>;
 
-export default TopArtists;
+import { ArtistCard , Error , Loader , SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/service/ShazamCore";
+
+
+const TopArtists  = () => {
+
+ 
+//  const [activeSong , isPlaying] = useSelector((state) => state.player );
+//  const [ data , isFetching , error] = useGetTopChartsQuery(country);
+
+
+
+//  if (isFetching) {
+//     <Loader title="Loading songs Top Charts"/>
+//  }
+//  if (error && country) {
+//    <Error/>
+//  }
+
+ return(
+      
+      <div className="flex flex-col">
+         <h2 className="font-bold text-3xl text-white text-left mb-10 mt-4">
+           Discover Top Artists
+            {/* <span className="text-black">{country}</span> */}
+         </h2>
+         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {/* {data.map(( track )=>(
+               <ArtistCard
+               key={song.Key}
+               song={song}
+               isPlaying={isPlaying}
+               activeSong={activeSong}
+               data={data}
+               i={i}
+
+                />
+            ))} */}
+         </div>
+      </div>
+ );
+};
+
+export default  TopArtists
+    
+
